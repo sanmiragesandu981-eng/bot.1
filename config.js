d0503f4356ff64d41df3002246ccf7b3bbbfac11
sanmiragesandu981-eng <sanmiragesandu981@gmail.com>
2025-08-26 17:44:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "GThm2QYQ#6ZJBJXeYLRDlrkffmQbSdeUxIxx2omEwbZdQ2MIMcwM",
   MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
   OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
 };
